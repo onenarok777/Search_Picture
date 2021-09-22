c5d4d690a0916d68d85c237a1080075d4eebf0fd
onenarok777 <onenarok777@gmail.com>
2021-09-23 05:51:56 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,12 +26,12 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    "@nuxtjs/vuetify",
-    "nuxt-vite"
+    "@nuxtjs/vuetify"
+    // "nuxt-vite"
   ],
-  vite: {
-    build: true
-  },
+  // vite: {
+  //   build: true
+  // },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
